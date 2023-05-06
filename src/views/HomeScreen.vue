4e4f42fc01d96d169c15aa52b8788613fca1ce39
sofialtman <70671805+sofialtman@users.noreply.github.com>
2023-05-05 23:21:34 -0300

    working on homeview

--- src/views/HomeScreen.vue
+++ src/views/HomeScreen.vue
@@ -9,42 +9,90 @@
       <v-list-item>
         <LogoComponent />
       </v-list-item>
-      <v-col mb-15 class="text-left" justify="space-between">
+      <v-col mb-15 class="text-left d-flex" justify="space-between">
         <v-row mb-5>
           <v-btn width="210" prepend-icon="mdi-home" variant="text" class="card_container text-left ml-5 mt-10 mb-5" style="justify-content: flex-start">Inicio</v-btn>
           <v-btn width="210" prepend-icon="mdi-devices" variant="text" class="card_container2 text-left ml-5 mt-5 mb-5" style="justify-content: flex-start">Mis Dispositivos</v-btn>
           <v-btn width="210" prepend-icon="mdi-clipboard-check-multiple" variant="text" class="card_container2 text-left ml-5 mt-5 mb-5" style="justify-content: flex-start">Mis Rutinas</v-btn>
-          <v-btn width="210" prepend-icon="mdi-help-circle-outline" variant="text" class="card_container2 text-left ml-5 mt-5 mb-5" style="justify-content: flex-start">Ayuda</v-btn>
-          <v-btn width="210" prepend-icon="mdi-close" variant="text" class="card_container3 text-left ml-5 mt-15 mb-5" style="justify-content: flex-start">Cerrar</v-btn>
-        </v-row>
+          <v-btn width="210" prepend-icon="mdi-help-circle-outline" variant="text" class="card_container2 text-left ml-5 mt-5 mb-16" style="justify-content: flex-start">Ayuda</v-btn>
+          <v-space></v-space>
+          <v-btn width="210" prepend-icon="mdi-close" variant="text" class="card_container3 text-left ml-5 mt-16" style="justify-content: flex-start" >Cerrar</v-btn>
+        </v-row>  
       </v-col>
     </v-navigation-drawer>
-
     <div class="upper_bar">
-      <v-row justify="space-between center" class="mt-1 mr-1">
-        <v-col></v-col>
-        <v-col cols="auto">
-          <v-btn icon="mdi-account" variant="text" size="large"/>
+      <v-toolbar>
+          <v-spacer></v-spacer>
+          <v-btn icon  class="mt-5">
+            <v-icon color="#146C94">mdi-account</v-icon>
+          </v-btn>
+      </v-toolbar>
+    </div>
+    <div>
+      <v-row cols="5">
+        <v-col>
+          <h2>FAVORITOS</h2>
+        </v-col>
+        <v-col>
+          <h2>DESTACADOS</h2>
         </v-col>
       </v-row>
     </div>
-    <v-menu>
-      <template v-slot:activator="{ props }">
-        <v-btn
-          color="primary"
-          v-bind="props">Activator slot
-        </v-btn>
-      </template>
-      <v-list>
-        <v-list-item
-          v-for="(item, index) in items"
-          :key="index"
-          :value="index">
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-menu>
-  </v-container>
+    <v-row>
+      <v-card class="card_container4 card_c1">
+        <v-row>
+        <v-card-actions>
+          <v-container>
+            <v-row dense>
+              <v-col cols="12">
+                <v-btn size="large" width="100" rounded="lg" variant="flat">Casa
+                  <template v-slot:append>
+                    <v-icon color="#146C94">mdi-heart</v-icon>
+                  </template>
+                </v-btn> 
+              </v-col>
+            
+            </v-row>
+            </v-container>
+        </v-card-actions>
+        </v-row>
+        <v-row>
+          <v-card-actions>
+          <v-container>
+            <v-row dense>
+              <v-col cols="12">
+                <v-btn size="large" width="100" rounded="lg" variant="flat">Hab1
+                  <template v-slot:append>
+                    <v-icon color="#146C94">mdi-heart</v-icon>
+                  </template>
+                </v-btn> 
+              </v-col>
+            </v-row>
+            </v-container>
+        </v-card-actions>
+        </v-row>
+        <v-row>
+          <v-card-actions>
+          <v-container>
+            <v-row dense>
+              <v-col cols="12">
+                <v-btn size="large" width="200" rounded="lg" variant="flat">Alarma1
+                  <template v-slot:append>
+                    <v-icon color="#146C94">mdi-heart</v-icon>
+                  </template>
+                </v-btn> 
+              </v-col>
+            </v-row>
+            </v-container>
+        </v-card-actions>
+        </v-row>
+      </v-card>
+      <v-card class="card_container4 card_c2">
+
+      </v-card>
+
+</v-row>
+</v-container>
 </template>
 
 <style scoped>
@@ -52,7 +100,7 @@
 .bg {
   width: 100%;
   height: 100%;
-  position: relative;
+  position: absolute;
   top: 0;
   left: 0;
   background-image: url("@/assets/homeBackground.jpeg");
@@ -63,30 +111,70 @@
   position: absolute;
   color: rgb(20, 108, 148);
   font-family: 'Varela Round', sans-serif;
-  background-color: #EDF1F4;
+  background-color: #FFF;
   width: 100%;
-  height: 11%;
+  height: 10%;
   top: 0;
   left: 0;
 }
+.v-toolbar{
+  height: 100%;
+  position: absolute;
+}
+.card_container4{
+  border-radius: 20px;
+  background-color: #d5dbe0;
+  height: 520px;
+}
+.card_container5{
+  background-color: #FFF;
+  width: 30px;
+}
+.card_cb1{
+  height:20px;
+}
+.card_cb2{
+  height: 40px;
+}
+.card_cb3{
+  height: 60px
+}
+.card_c1{
+  margin-left: 280px;
+  width: 700px;
+}
+.card_c2{
+  margin-left: 10px;
+  width: 400px;
+}
 .card_container {
   color: rgb(20, 108, 148);
-  font-family: 'Varela Round', sans-serif;
+  font-family: 'Varela Round', sans-serif, bold;
   border-radius: 20px;
   background-color: #29ABE2;
   padding-left: 5;
-  align-items: align-left;
+  align-items: text-left, flex-start;
 }
 .card_container2 {
   color: #FFF;
   font-family: 'Varela Round', sans-serif;
   border-radius: 20px;
-  align-items: align-left
+  align-items: text-left, flex-start;
 }
 .card_container3 {
   color: #29ABE2;
   font-family: 'Varela Round', sans-serif;
   border-radius: 20px;
-  align-items: align-left
+  align-items: text-left, flex-start;
 }
+h2{
+  color: #FFF;
+  font-family: 'Varela Round', sans-serif;
+  font-size: 32px;
+  margin-top: 115px;
+  margin-bottom: 2px;
+  margin-left: 300px;
+}
+
+
 </style>
