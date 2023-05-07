098ca3ad2fe23e65abdb47ffbef1a12b4100559a
marianoagopian <agopian.marian@gmail.com>
2023-05-07 16:53:31 -0300

    removed navbar from app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <script setup>
 import { useRouter } from 'vue-router';
 import { computed } from 'vue';
-import NavBarComponent from '@/components/NavBarComponent.vue'
-
-const showNavBar = computed(() => {
-  return useRouter().currentRoute.value.path !== '/';
-});
 
 </script>
 
 <template>
   <div id="app">
     <v-app>
-      <NavBarComponent v-if="showNavBar"/>
       <RouterView/>
     </v-app>
   </div>
