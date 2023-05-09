37e6bd7376d5d4dc4d6fad2c43e7056a5287556e
sofialtman <70671805+sofialtman@users.noreply.github.com>
2023-05-09 18:20:41 -0300

    Fixing bugs

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -1,47 +1,17 @@
 <script setup>
 import NavBarComponent from '@/components/NavBarComponent.vue';
-const devices = [
-    { title: 'Luz 1', icon:'mdi-lightbulb' ,state_light: 'Apagado', state_color:'purple', intensity:'50%'},
-    /*{ title: 'Luz 2', icon:'mdi-lightbulb' ,state_light: 'Apagado', state_color:'purple', intensity:'50'},*/
-]
+const devices = ref([
+{ open: false, title: 'Luz 1', icon:'mdi-lightbulb', statelight: 'Apagada', red: 50, blue: 0, green: 0 , cardColor: 'rgb(0, 0, 0)'},
+{ open: false, title: 'Luz 2', icon:'mdi-lightbulb', statelight: 'Apagada', red: 50, blue: 0, green: 0, cardColor: 'rgb(0, 0, 0)' },
+])/*
+var devices = [
+    { menu: ref(false), title: 'Luz 1', icon:'mdi-lightbulb' ,state_light: 'Apagado', red: 50, blue:0, green:0, intensity:'50%'},
+    /*{ menu: ref(false), title: 'Luz 2', icon:'mdi-lightbulb' ,state_light: 'Apagado', red: 0, blue:0, green:0, intensity:'50'},*/
+//] 
 import { ref } from 'vue';
 const menu = ref(false);
-const light1 = ref({
-  red: 0,
-  blue: 0,
-  green: 0,
-})
 
-const menu2 = {
-  data() {
-    return {
-      colorMenu: false,
-      props: {
-    color: {
-      type: String,
-      default: "#ffffff",
-    },
-    slider1: {
-      type: Number,
-      default: 50,
-    },
-    slider2: {
-      type: Number,
-      default: 50,
-    },
-  },
-  methods: {
-    toggleColorMenu() {
-      this.colorMenu = !this.colorMenu
-    },
-    setColor(color) {
-      document.documentElement.style.setProperty('--primary-color', color);
-      this.toggleColorMenu();
-    }
-  }
-}
-  }
-}
+
 </script>
 
 <template>
@@ -63,14 +33,14 @@ const menu2 = {
                             </v-btn>
                         </template>
                     </v-list-item>
-                        <v-list class="horizontal_v_list">
+                        <v-list class="horizontal_v_list d-flex align-start">
                             <v-list-item active="false" class="horizontal_v_list_card flex-column text-left"
                                 v-for="(item, index) in devices"
                                     :key="index"
                                     :value="index"
                                 >
                                 <v-list-item >
-                                <v-list-item-title flexibility="space-between" ml-10 v-text="item.title"></v-list-item-title> 
+                                <v-list-item-title flexibility="space-between"  v-text="item.title"></v-list-item-title> 
                                 <template v-slot:prepend>
                                         <v-icon color="#146C94">{{item.icon}}</v-icon>
                                 </template>
@@ -79,21 +49,24 @@ const menu2 = {
                                 </template>
                                 </v-list-item>
                                 <v-divider></v-divider>
-                                <v-list-item class="mb-1">
-                                    <v-list-item  v-text="item.state_light"></v-list-item>
+                                <v-row>
+                                <v-list-item >
+                                    <v-list-item  v-text="item.statelight"></v-list-item>
                                     <template v-slot:append>
-                                        <v-switch class="mt-3"></v-switch>
+                                        <v-switch class="mt-5"></v-switch>
                                     </template>
                                 </v-list-item>
+                            </v-row>
                                 <v-divider></v-divider>
-                                <v-list-item class="mt-2 mb-2">
+                                <v-list-item class="mt-4 mb-4">
                                 <!--<v-list-subtitle>{{ item.state_color }}</v-list-subtitle>-->
                                 <div>
-                                    <v-menu background-color="black" v-model="menu" :close-on-content-click="false" location="end" >
+                                    <v-row>
+                                    <v-menu  v-model="item.open" :close-on-content-click="false" location="end" >
                                         <template v-slot:activator="{ props }" v-slot:prepend>
-                                            <v-btn class="container_button" v-bind="props"></v-btn>
+                                            <v-btn :style="{ background: `rgb(${item.red}, ${item.green}, ${item.blue})` }" class="container_button" v-bind="props"></v-btn>
                                         </template>
-                                        <div class="menu2">
+                                        <div>
                                         <v-card min-width="250">
                                             <v-list>
                                                 <v-list-item class="pr-7 pl-5" >
@@ -102,24 +75,24 @@ const menu2 = {
                                             </v-list>
                                             <v-divider></v-divider>
                                             <v-list>
-                                                <v-list-item class="menu2" >
+                                                <v-list-item>
                                                     <v-row align="center" justify="space-between" no-gutters>
                                                         <v-card width="400" height="370" style="margin: auto">
-                                                            <v-responsive :style="{ background: `rgb(${light1.red}, ${light1.green}, ${light1.blue})` }" height="150px"></v-responsive>
+                                                            <v-responsive :style="{ background: `rgb(${item.red}, ${item.green}, ${item.blue})` }" height="150px"></v-responsive>
                                                             <v-card-text class="menu2">
-                                                                    <v-slider v-model="light1.red" :max="255" :step="1" label="R" hide-details class="ma-4">
+                                                                    <v-slider v-model="item.red" :max="255" :step="1" label="R" hide-details class="ma-4" @input="updateColor">
                                                                         <template v-slot:append>
-                                                                            <v-text-field v-model="light1.red" type="number" style="width: 80px" density="compact" hide-details variant="outlined" ></v-text-field>
+                                                                            <v-text-field v-model="item.red" type="number" style="width: 80px" density="compact" hide-details variant="outlined" ></v-text-field>
                                                                         </template>
                                                                     </v-slider>
-                                                                    <v-slider v-model="light1.green" :max="255" :step="1" label="G" hide-details class="ma-4">
+                                                                    <v-slider v-model="item.green" :max="255" :step="1" label="G" hide-details class="ma-4" @input="updateColor">
                                                                         <template v-slot:append>
-                                                                            <v-text-field v-model="light1.green" type="number" style="width: 80px" density="compact" hide-details variant="outlined" ></v-text-field>
+                                                                            <v-text-field v-model="item.green" type="number" style="width: 80px" density="compact" hide-details variant="outlined" ></v-text-field>
                                                                         </template>
                                                                     </v-slider>
-                                                                    <v-slider v-model="light1.blue" :max="255" :step="1" label="B" hide-details class="ma-4">
+                                                                    <v-slider v-model="item.blue" :max="255" :step="1" label="B" hide-details class="ma-4" @input="updateColor">
                                                                         <template v-slot:append>
-                                                                            <v-text-field v-model="light1.blue" type="number" style="width: 80px" density="compact" hide-details variant="outlined" ></v-text-field>
+                                                                            <v-text-field v-model="item.blue" type="number" style="width: 80px" density="compact" hide-details variant="outlined" ></v-text-field>
                                                                         </template>
                                                                     </v-slider>
                                                             </v-card-text>
@@ -129,21 +102,32 @@ const menu2 = {
                                                 </v-list>
                                                 <v-card-actions>
                                                     <v-spacer></v-spacer>
-                                                   <v-btn color="primary" variant="text" @click="menu = false">Cerrar</v-btn>
+                                                   <v-btn color="primary" variant="text" v-model="item.open" :close-on-content-click="true">Actualizar</v-btn>
+                                                   <!-- no me esta cerrando pero problemas del futuro -->
                                                 </v-card-actions>
                                             </v-card>
                                         </div>
                                     </v-menu>
+                                    <v-list-item>
+                                        <p class="text_rgb mr-1">R: {{ item.red }} ; G: {{ item.green }} ; B: {{ item.blue }}</p>
+                                    </v-list-item>
+                                </v-row>
                                 </div>
                             </v-list-item>
                             <v-divider></v-divider>
-                            <v-list-item>
+                            <v-list-item class="mt-5 mb-3">
                             <v-list-subtitle>Intensidad: </v-list-subtitle>
                             <v-list-subtitle>{{ item.intensity }}</v-list-subtitle>
                                 <v-slider width="30px" thumb-label="always"></v-slider>
                             </v-list-item>
                             </v-list-item>
+                            <v-list-item>
+                                <v-footer class="add-button-container" absolute>
+                        <v-btn variant="flat" icon="mdi-plus-circle-outline" size="50px"></v-btn>   <!-- hay q cambiar la accion del click-->
+                    </v-footer>
+                            </v-list-item>
                         </v-list>
+                    
                 </v-card> 
             </v-container> 
         </v-main>
@@ -198,7 +182,7 @@ h2{
   width: 250px;
   box-shadow: 3px 4px 5px #6394a2;
   transform: translateZ(0);
-  height: 300px;
+  height: 400px;
   
 }
 
@@ -216,5 +200,15 @@ h2{
     height: 520px;
     width: 800px;
   }
-
+  .add-button-container {
+  position: fixed;
+  bottom: 16px;
+  right: 32px;
+  background-color: transparent;
+}
+.text_rgb{
+    font-family: 'Varela Round', sans-serif;
+    font-size: 10px;
+    color:  #76797c;
+}
 </style>
