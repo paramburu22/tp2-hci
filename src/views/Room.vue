baa528a827ef877dc487a0f7d8ae565a65c4cad8
sofialtman <70671805+sofialtman@users.noreply.github.com>
2023-05-11 14:41:31 -0300

    improving room and implementing lightComponent

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -1,8 +1,8 @@
 <script setup>
 import NavBarComponent from '@/components/NavBarComponent.vue';
 const devices = ref([
-{ open: false, title: 'Luz 1', icon:'mdi-lightbulb', statelight: 'Apagada', red: 0, blue: 0, green: 0 , hexa:'#000000', cardColor: 'rgb(0, 0, 0)', intensity: 0},
-{ open: false, title: 'Luz 2', icon:'mdi-lightbulb', statelight: 'Apagada', red: 0, blue: 0, green: 0, hexa:'#000000', cardColor: 'rgb(0, 0, 0)', intensity: 0 },
+{ open: false, title: 'Luz 1', icon:'mdi-lightbulb', model: 'Apagada', red: 0, blue: 0, green: 0 , hexa:'#000000', cardColor: 'rgb(0, 0, 0)', intensity: 0},
+{ open: false, title: 'Luz 2', icon:'mdi-lightbulb', model: 'Apagada', red: 0, blue: 0, green: 0, hexa:'#000000', cardColor: 'rgb(0, 0, 0)', intensity: 0 },
 ])
 import { ref, computed } from 'vue';
 
@@ -68,7 +68,11 @@ const computedDevices = computed(() => {
                                 <v-list-item >
                                     <v-list-item  v-text="item.statelight"></v-list-item>
                                     <template v-slot:append>
-                                        <v-switch class="mt-5"></v-switch>
+                                        <v-switch class="mt-1 mb-1" v-model="item.model" hide-details
+                                            true-value="Prendido"
+                                            false-value="Apagado"
+                                            :label="`${item.model}`">
+                                        </v-switch>
                                     </template>
                                 </v-list-item>
                             </v-row>
@@ -115,7 +119,7 @@ const computedDevices = computed(() => {
                                         </v-list>
                                         <v-card-actions>
                                             <v-spacer></v-spacer>
-                                            <v-btn color="primary" variant="text" v-model="item.open" :close-on-content-click="true">Actualizar</v-btn>
+                                            <v-btn color="primary" variant="text"  @click="toggleOpen" >Actualizar</v-btn>
                                         </v-card-actions>
                                     </v-card>
                                 </div>
@@ -192,7 +196,6 @@ h2{
   width: 270px;
   box-shadow: 3px 4px 5px #6394a2;
   transform: translateZ(0);
-  
 }
 
 .list_card{
